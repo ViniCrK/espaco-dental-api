7b5bdfb76d5b839843325fc301041c7022defd88
ViniCrK <vinicrk@outlook.com>
2024-09-18 10:45:18 -0300

    Add JWT authentication

--- appointments/views.py
+++ appointments/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
 
 from .serializers.appointment_serializers import AppointmentSerializer
@@ -8,5 +9,6 @@ from .models import Appointment
 class AppointmentViewSet(ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields = ['appointment_occasion', 'status',]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import filters
 
 from .serializers.patient_serializers import PatientSerializer
@@ -10,6 +11,7 @@ from .models import Patient, Dentist
 class PatientViewSet(ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = [IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
 
@@ -17,5 +19,6 @@ class PatientViewSet(ModelViewSet):
 class DentistViewSet(ModelViewSet):
     queryset = Dentist.objects.all()
     serializer_class = DentistSerializer
+    permission_classes = {IsAuthenticated}
     filter_backends = [filters.SearchFilter]
     search_fields = ['name']
